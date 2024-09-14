097f69e22417b5bf9ba6b80679cb82adcd5bd983
ZahirKhan97 <GitHub: mzahirkhan302@gmail.com>
2024-09-15 02:33:22 +0500

    Post: CRUD API's Completed

--- app/Http/Controllers/API/PostController.php
+++ app/Http/Controllers/API/PostController.php
@@ -3,16 +3,21 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\API\BaseController as BaseController;
+use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
-class PostController extends Controller
+class PostController extends BaseController
 {
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        $data['posts'] = Post::all();
+
+        return $this->sendResponse($data, 'All Posts Data');
     }
 
     /**
@@ -20,7 +25,35 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'title' => 'required',
+            'description' => 'required',
+            'image' => 'required|mimes:jpg,jpeg,gif,png',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Validation Errors',
+                'errors' => $validator->errors()->all()
+            ], 401);
+
+            return $this->sendError('Validation Errors', $validator->errors()->all(), 401);
+        }
+
+        $img = $request->file('image');
+        $ext = $img->getClientOriginalExtension();
+        $imageName = time() . '.' . $ext;
+        $img->move(public_path() . '/uploads/', $imageName);
+
+        $post = Post::create([
+            'title' => $request->title,
+            'description' => $request->description,
+            'image' => $imageName
+        ]);
+
+
+        return $this->sendResponse($post, 'Post Created Successfully');
     }
 
     /**
@@ -28,7 +61,9 @@ class PostController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $data['post'] = Post::where(['id' => $id])->get();
+
+        return $this->sendResponse($data, 'Single Post');
     }
 
     /**
@@ -36,7 +71,41 @@ class PostController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'title' => 'required',
+            'description' => 'required',
+            'image' => 'required|mimes:jpg,jpeg,gif,png',
+        ]);
+
+        if ($validator->fails()) {
+
+            return $this->sendError('Validation Errors', $validator->errors()->all(), 401);
+        }
+        $postImage = Post::select('id', 'image')->where(['id' => $id])->get();
+        if ($request->image != '') {
+            $path = public_path() . '/uploads/';
+            if ($postImage[0]->image != '' && $postImage[0]->image != null) {
+                $old_file = $path . $postImage[0]->image;
+                if (file_exists($old_file)) {
+                    unlink($old_file);
+                }
+            }
+            $img = $request->file('image');
+            $ext = $img->getClientOriginalExtension();
+            $imageName = time() . '.' . $ext;
+            $img->move(public_path() . '/uploads', $imageName);
+        } else {
+            $imageName = $postImage[0]->image;
+        }
+
+
+        $post = Post::where(['id' => $id])->update([
+            'title' => $request->title,
+            'description' => $request->description,
+            'image' => $imageName
+        ]);
+
+        return $this->sendResponse($post, 'Post updated Successfully');
     }
 
     /**
@@ -44,6 +113,11 @@ class PostController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $imagePath = Post::select('image')->where(['id' => $id])->get();
+        $file_path = public_path() . '/uploads/' . $imagePath[0]['image'];
+        unlink($file_path);
+        $post = Post::where(['id' => $id])->delete();
+
+        return $this->sendResponse($post, 'Your Post has been removed');
     }
 }
